4f1297dfa84ba764173cb04990d03efe925c4b71
huigeng-x <huigeng97@gmail.com>
2024-09-11 15:19:25 -0700

    fix nit

--- app/page.tsx
+++ app/page.tsx
@@ -49,16 +49,6 @@ function TextSection(title: string, body: string) {
   return (
     <div className="flex-auto flex flex-col mt-10">
       <h1 className="mt-2 mb-2 font-semibold tracking-tighter">{title}</h1>
-      {/* <p className="">
-          {`I built this site to transform my aversion to portfolio updates into a passion for continuous 
-        learning and sharing, creating a space I'm eager to revisit and enhance. By showcasing everything 
-        I’m passionate about to a broader audience, I strive to inspire others while holding myself 
-        accountable to high standards of creativity and growth.
-This site is curated with the same care and personality I'd put into decorating my home. It's a space that 
-reflects who I am and what I do.
-I've always found it fascinating to get to know someone through a well-maintained personal website. So
-whether you are a stranger, a friend, or somewhere in between – welcome. Make yourself comfortable in my digital home. `}
-        </p> */}
       <p className="mb-4 tracking-tighter">{body}</p>
     </div>
   );
